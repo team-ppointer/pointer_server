3569215fab2c87a767fb73398078c1b69dfee391
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-10-03 14:49:37 +0900

    [fix] 기본 테스트 삭제

--- src/test/java/com/moplus/moplus_server/MoplusServerApplicationTests.java
+++ src/test/java/com/moplus/moplus_server/MoplusServerApplicationTests.java
@@ -3,7 +3,7 @@ package com.moplus.moplus_server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MoplusServerApplicationTests {
 
     @Test
