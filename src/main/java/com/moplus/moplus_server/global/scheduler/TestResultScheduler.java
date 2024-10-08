ec9da11ae25eab8bd9e59cea17539fbe70d5f17e
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-10-08 20:24:54 +0900

    [fix] cd 디렉토리 변경

--- src/main/java/com/moplus/moplus_server/global/scheduler/TestResultScheduler.java
+++ src/main/java/com/moplus/moplus_server/global/scheduler/TestResultScheduler.java
@@ -13,7 +13,8 @@ public class TestResultScheduler {
 
     private final TestResultService testResultService;
 
-//    // 5분마다 실행 (cron 표현식을 사용해 5분마다 스케줄링)
+
+    // 5분마다 실행 (cron 표현식을 사용해 5분마다 스케줄링)
 //    @Scheduled(cron = "0 */5 * * * *")
 //    public void calculateAverageSolvingTime() {
 //        Duration averageDuration = testResultService.calculateAverageSolvingTime();
