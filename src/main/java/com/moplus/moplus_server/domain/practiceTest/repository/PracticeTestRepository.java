597adbb8ec56b83f09476915a2d4364f932f3110
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-11-10 02:38:32 +0900

    [chore] 로깅 레벨 변경

--- src/main/java/com/moplus/moplus_server/domain/practiceTest/repository/PracticeTestRepository.java
+++ src/main/java/com/moplus/moplus_server/domain/practiceTest/repository/PracticeTestRepository.java
@@ -6,11 +6,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PracticeTestRepository extends JpaRepository<PracticeTest, Long> {
     List<PracticeTest> findAllByOrderByViewCountDesc();
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select s from PracticeTest s where s.id = :id")
-    PracticeTest findByIdWithPessimisticLock(Long id);
+    PracticeTest findByIdWithPessimisticLock(@Param("id") Long id);
 }
