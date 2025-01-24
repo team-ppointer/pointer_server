41b4b4e9d6cf3d1750fe7902c7e10d886abeff84
박세준 <74056843+sejoon00@users.noreply.github.com>
2025-01-24 20:20:05 +0900

    [feat/#17] 내 정보 조회 api 추가

--- src/main/java/com/moplus/moplus_server/global/config/WebConfig.java
+++ src/main/java/com/moplus/moplus_server/global/config/WebConfig.java
@@ -1,9 +1,12 @@
 package com.moplus.moplus_server.global.config;
 
+import com.moplus.moplus_server.global.annotation.AuthenticationArgumentResolver;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,15 +16,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableTransactionManagement
 public class WebConfig implements WebMvcConfigurer {
 
+    private final AuthenticationArgumentResolver authenticationArgumentResolver;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://dev.mopl.kr","http://dev.mopl.kr", "http://localhost:8080", "https://www.mopl.kr", "http"
-                + "://localhost:3000", "https://dev-web.mopl.kr", "http://dev-web.mopl.kr")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true);
+                .allowedOrigins("https://dev.mopl.kr", "http://dev.mopl.kr", "http://localhost:8080",
+                        "https://www.mopl.kr", "http"
+                                + "://localhost:3000", "https://dev-web.mopl.kr", "http://dev-web.mopl.kr")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authenticationArgumentResolver);
+    }
 }
