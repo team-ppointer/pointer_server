e778a6982c587dea8d9a661cf980609dfeda5bb8
박세준 <74056843+sejoon00@users.noreply.github.com>
2025-01-27 00:26:56 +0900

    [feat/#21]문항, 새끼문항, 개념태그 엔티티 구현

---
+++ src/main/java/com/moplus/moplus_server/domain/problem/domain/Problem.java
@@ -0,0 +1,47 @@
+package com.moplus.moplus_server.domain.problem.domain;
+
+import com.moplus.moplus_server.global.common.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Problem extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "problem_id")
+    Long id;
+
+    String practiceTestId;
+    int number;
+    int answer;
+    String comment;
+    String mainProblemImageUrl;
+    String mainAnalysisImageUrl;
+    String readingTipImageUrl;
+    String seniorTipImageUrl;
+    String prescriptionImageUrl;
+
+    @ElementCollection
+    @CollectionTable(name = "problem_concept", joinColumns = @JoinColumn(name = "concept_tag_id"))
+    Set<Long> conceptTagIds;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "problem_id")
+    private List<ChildProblem> childProblems = new ArrayList<>();
+}
