c389d797b39b171585f9051c9da07849f720e8de
박세준 <74056843+sejoon00@users.noreply.github.com>
2025-01-30 22:45:04 +0900

    [feature/#23] 모의고사 목록 조회 swagger 설명 추가

--- src/main/java/com/moplus/moplus_server/domain/problem/controller/PracticeTestTagController.java
+++ src/main/java/com/moplus/moplus_server/domain/problem/controller/PracticeTestTagController.java
@@ -2,6 +2,7 @@ package com.moplus.moplus_server.domain.problem.controller;
 
 import com.moplus.moplus_server.domain.problem.dto.response.PracticeTestTagResponse;
 import com.moplus.moplus_server.domain.problem.repository.PracticeTestTagRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,7 @@ public class PracticeTestTagController {
     private final PracticeTestTagRepository practiceTestTagRepository;
 
     @GetMapping("")
+    @Operation(summary = "모의고사 목록 조회")
     public ResponseEntity<List<PracticeTestTagResponse>> getPracticeTestTags() {
         List<PracticeTestTagResponse> responses = practiceTestTagRepository.findAll().stream()
                 .map(PracticeTestTagResponse::of)
