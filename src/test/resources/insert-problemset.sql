fbf55201a80a003d8d217ddffbd44c5827d4e80b
HongGit <soojam@konkuk.ac.kr>
2025-02-02 19:15:48 +0900

    [fix/#31] 테스트 문항 데이터 null값 삭제

--- src/test/resources/insert-problemset.sql
+++ src/test/resources/insert-problemset.sql
@@ -35,6 +35,6 @@ VALUES ('24052001001', 1, 1, '1', '기존 문제 설명',
 INSERT INTO problem (problem_id, practice_test_id, number, answer, comment, main_problem_image_url,
                      main_analysis_image_url, reading_tip_image_url, senior_tip_image_url, prescription_image_url,
                      is_published, is_variation)
-VALUES ('24052001004', 1, 4, NULL, '유효하지 않은 문제 설명',
-        NULL, 'mainAnalysis4.png', 'readingTip4.png', 'seniorTip4.png', 'prescription4.png',
+VALUES ('24052001004', 1, 4, '', '유효하지 않은 문제 설명',
+        '', 'mainAnalysis4.png', 'readingTip4.png', 'seniorTip4.png', 'prescription4.png',
         false, false);
