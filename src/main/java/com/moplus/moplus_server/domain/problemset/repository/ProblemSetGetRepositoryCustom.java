abab3c3fbf9c25e7276859cde3dd97cce628360a
HongGit <soojam@konkuk.ac.kr>
2025-03-12 18:01:48 +0900

    [fix/#91] Q클래스 import문 추가

--- src/main/java/com/moplus/moplus_server/domain/problemset/repository/ProblemSetGetRepositoryCustom.java
+++ src/main/java/com/moplus/moplus_server/domain/problemset/repository/ProblemSetGetRepositoryCustom.java
@@ -1,10 +1,11 @@
 package com.moplus.moplus_server.domain.problemset.repository;
 
+import static com.moplus.moplus_server.admin.publish.domain.QPublish.*;
+import static com.moplus.moplus_server.domain.concept.domain.QConceptTag.*;
+import static com.moplus.moplus_server.domain.problem.domain.problem.QProblem.*;
+
 import com.moplus.moplus_server.admin.problemset.dto.response.ProblemSetGetResponse;
 import com.moplus.moplus_server.admin.problemset.dto.response.ProblemSummaryResponse;
-import com.moplus.moplus_server.admin.publish.domain.QPublish;
-import com.moplus.moplus_server.domain.concept.domain.QConceptTag;
-import com.moplus.moplus_server.domain.problem.domain.problem.QProblem;
 import com.moplus.moplus_server.domain.problemset.domain.ProblemSet;
 import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -22,51 +23,51 @@ public class ProblemSetGetRepositoryCustom {
     public ProblemSetGetResponse getProblemSet(ProblemSet problemSet) {
         // 발행 날짜 조회 쿼리
         List<LocalDate> publishedDates = queryFactory
-                .select(QPublish.publish.publishedDate)
-                .from(QPublish.publish)
-                .where(QPublish.publish.problemSetId.eq(problemSet.getId()))
+                .select(publish.publishedDate)
+                .from(publish)
+                .where(publish.problemSetId.eq(problemSet.getId()))
                 .fetch();
 
         // 문제 조회 쿼리 (문제 자체 정보만 조회)
         List<Tuple> problemData = queryFactory
                 .select(
-                        QProblem.problem.id,
-                        QProblem.problem.problemCustomId.id,
-                        QProblem.problem.title.title,
-                        QProblem.problem.memo,
-                        QProblem.problem.mainProblemImageUrl
+                        problem.id,
+                        problem.problemCustomId.id,
+                        problem.title.title,
+                        problem.memo,
+                        problem.mainProblemImageUrl
                 )
-                .from(QProblem.problem)
-                .where(QProblem.problem.id.in(problemSet.getProblemIds()))
+                .from(problem)
+                .where(problem.id.in(problemSet.getProblemIds()))
                 .distinct()
                 .fetch();
 
         // 태그 조회 쿼리 (각 문제별 태그만 조회)
         Map<Long, Set<String>> conceptTagMap = queryFactory
-                .select(QProblem.problem.id, QConceptTag.conceptTag.name)
-                .from(QProblem.problem)
-                .leftJoin(QConceptTag.conceptTag)
-                .on(QConceptTag.conceptTag.id.in(QProblem.problem.conceptTagIds))
-                .where(QProblem.problem.id.in(problemSet.getProblemIds()))
+                .select(problem.id, conceptTag.name)
+                .from(problem)
+                .leftJoin(conceptTag)
+                .on(conceptTag.id.in(problem.conceptTagIds))
+                .where(problem.id.in(problemSet.getProblemIds()))
                 .fetch()
                 .stream()
                 .collect(
                         HashMap::new,
                         (map, tuple) -> map
-                                .computeIfAbsent(tuple.get(QProblem.problem.id), k -> new HashSet<>())
-                                .add(tuple.get(QConceptTag.conceptTag.name)),
+                                .computeIfAbsent(tuple.get(problem.id), k -> new HashSet<>())
+                                .add(tuple.get(conceptTag.name)),
                         HashMap::putAll
                 );
 
         // 문제 요약 정보 생성
         List<ProblemSummaryResponse> problemSummaries = problemData.stream()
                 .map(tuple -> ProblemSummaryResponse.builder()
-                        .problemId(tuple.get(QProblem.problem.id))
-                        .problemCustomId(tuple.get(QProblem.problem.problemCustomId.id))
-                        .problemTitle(tuple.get(QProblem.problem.title.title))
-                        .memo(tuple.get(QProblem.problem.memo))
-                        .mainProblemImageUrl(tuple.get(QProblem.problem.mainProblemImageUrl))
-                        .tagNames(conceptTagMap.getOrDefault(tuple.get(QProblem.problem.id), new HashSet<>()))
+                        .problemId(tuple.get(problem.id))
+                        .problemCustomId(tuple.get(problem.problemCustomId.id))
+                        .problemTitle(tuple.get(problem.title.title))
+                        .memo(tuple.get(problem.memo))
+                        .mainProblemImageUrl(tuple.get(problem.mainProblemImageUrl))
+                        .tagNames(conceptTagMap.getOrDefault(tuple.get(problem.id), new HashSet<>()))
                         .build()
                 )
                 .toList();
