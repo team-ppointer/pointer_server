c5952555ef4c32e676744c7eded4c69c613a45be
HongGit <soojam@konkuk.ac.kr>
2025-03-25 19:25:32 +0900

    [hotfix] 문항조회 필수 필드 표시

--- src/main/java/com/moplus/moplus_server/client/problem/dto/response/ProblemClientGetResponse.java
+++ src/main/java/com/moplus/moplus_server/client/problem/dto/response/ProblemClientGetResponse.java
@@ -4,17 +4,25 @@ import com.moplus.moplus_server.client.submit.domain.ChildProblemSubmitStatus;
 import com.moplus.moplus_server.client.submit.domain.ProblemSubmitStatus;
 import com.moplus.moplus_server.domain.problem.domain.problem.AnswerType;
 import com.moplus.moplus_server.domain.problem.domain.problem.Problem;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record ProblemClientGetResponse(
+        @NotNull(message = "문항번호는 필수입니다.")
         int number,
+        @NotNull(message = "문항이미지는 필수입니다.")
         String imageUrl,
+        @NotNull(message = "추천시간은 필수입니다.")
         Integer recommendedMinute,
+        @NotNull(message = "추천시간은 필수입니다.")
         Integer recommendedSecond,
+        @NotNull(message = "문항제출 상태는 필수입니다.")
         ProblemSubmitStatus status,
+        @NotNull(message = "새끼문항제출 상태는 필수입니다.")
         List<ChildProblemSubmitStatus> childProblemStatuses,
+        @NotNull(message = "답변타입은 필수입니다.")
         AnswerType answerType
 ) {
     public static ProblemClientGetResponse of(Problem problem, ProblemSubmitStatus status,
