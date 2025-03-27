1d7c861f3602bf928a35446befe1d59d8e35cb1e
HongGit <soojam@konkuk.ac.kr>
2025-03-27 18:56:36 +0900

    [hotfix] 새끼문항 조회 응답 필수값 처리

--- src/main/java/com/moplus/moplus_server/client/problem/dto/response/ChildProblemClientGetResponse.java
+++ src/main/java/com/moplus/moplus_server/client/problem/dto/response/ChildProblemClientGetResponse.java
@@ -1,13 +1,18 @@
 package com.moplus.moplus_server.client.problem.dto.response;
 
 import com.moplus.moplus_server.client.submit.domain.ChildProblemSubmitStatus;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record ChildProblemClientGetResponse(
+        @NotNull(message = "문항번호는 필수입니다.")
         int problemNumber,
+        @NotNull(message = "새끼문항번호는 필수입니다.")
         int childProblemNumber,
+        @NotNull(message = "이미지URL은 필수입니다.")
         String imageUrl,
+        @NotNull(message = "새끼문항 제출상태는 필수입니다.")
         ChildProblemSubmitStatus status
 ) {
     public static ChildProblemClientGetResponse of(int problemNumber, int childProblemNumber, String imageUrl,
